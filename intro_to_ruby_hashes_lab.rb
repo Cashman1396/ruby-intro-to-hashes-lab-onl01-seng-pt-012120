736dea3a47029ed5b22c91aeb47724734adc78f1
Chris Ashman <chrisashman96@gmail.com>
2020-01-29 21:28:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,10 +15,10 @@ end
 
 def monopoly_with_second_tier
 		monopoly = {
-	  :railroads => {
-	    :pieces: 4,
-	    :name: {},
-	    :rent_in_dollars: {}
+	  railroads => {
+	    :pieces => 4,
+	    :name => {},
+	    :rent_in_dollars=> {}
 	    
 	  }
 	}
